6e37fab8f10cfab37d642d9dc8174c567f40dc4a
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2024-12-18 11:28:59 +0530

    Create LOAD THE DATASET.py

---
+++ LOAD THE DATASET.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+# load the dataset to variable
+df = pd.read_csv()
+# check the 5 rows of dataset
+df.head()
